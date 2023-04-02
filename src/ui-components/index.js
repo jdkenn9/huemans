5c32e139e718103d549816e7995f0fe1439994d9
Jailynn <72613082+JailynnPrimus@users.noreply.github.com>
2023-04-02 17:36:15 -0400

    Revert "header, footer, profile, and favorites components added"
    This reverts commit b386168b7932e656f91d54a56d200e2befdfcacc.

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,23 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-export { default as Component1 } from "./Component1";
-export { default as Navs } from "./Navs";
-export { default as HuemansLogo } from "./HuemansLogo";
-export { default as Header } from "./Header";
-export { default as SuggestedBusinesses } from "./SuggestedBusinesses";
-export { default as Favorites } from "./Favorites";
-export { default as Title } from "./Title";
-export { default as ItemCard } from "./ItemCard";
-export { default as NavMenuOptions } from "./NavMenuOptions";
-export { default as MenuItem } from "./MenuItem";
-export { default as Search } from "./Search";
-export { default as Footer } from "./Footer";
-export { default as User } from "./User";
 export { default as studioTheme } from "./studioTheme";
-export { default as UsersCreateForm } from "./UsersCreateForm";
-export { default as UsersUpdateForm } from "./UsersUpdateForm";
-export { default as NotesCreateForm } from "./NotesCreateForm";
-export { default as NotesUpdateForm } from "./NotesUpdateForm";
-export { default as LogosCreateForm } from "./LogosCreateForm";
-export { default as LogosUpdateForm } from "./LogosUpdateForm";
+export { default as NoteCreateForm } from "./NoteCreateForm";
+export { default as NoteUpdateForm } from "./NoteUpdateForm";
